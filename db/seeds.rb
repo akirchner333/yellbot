f3f7a025d0d781156b68db8179bcb5df4ba7683d
Alexa Kirchner <akirchner333@users.noreply.github.com>
2024-08-06 14:51:10 -0500

    Uses seeds to upload banlist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+File.open("ban.txt", "r") do |f|
+	f.each_line do |host|
+		BanHost.find_or_create_by(name: host.sub("\n", ""))
+	end
+end
